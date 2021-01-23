f5bbb500d5f7d181537bfce3a8a9a203ed5be6df
Sebastian S. Cocioba <sebastian@binomicalabs.org>
2021-01-22 21:27:25 -0500

    functional camera and minimap!

--- main.lua
+++ main.lua
@@ -1,12 +1,95 @@
 local gamera = require 'gamera'
-local cam = gamera.new(0,0,8000,8000)
-cam:setWindow(0,0,800,600)
+
+local min, max = math.min, math.max
+
+-- game variables (entities)
+local world, player, target, cam1, cam2
+
+-- auxiliary functions
+local isDown = love.keyboard.isDown
+local floor = math.floor
+
+local function makeZero(x,minX,maxX)
+  if x > maxX or x < minX then return x end
+  return 0
+end
+
+local function drawTarget()
+    love.graphics.setColor(255,255,0)
+    love.graphics.rectangle("fill", target.x, target.y, 32, 32)
+  end
+
+local function drawWorld(cl,ct,cw,ch)
+    local w = world.w / world.columns
+    local h = world.h / world.rows
+  
+    local minX = max(floor(cl/w), 0)
+    local maxX = min(floor((cl+cw)/w), world.columns-1)
+    local minY = max(floor(ct/h), 0)
+    local maxY = min(floor((ct+ch)/h), world.rows-1)
+  
+    for y=1, #map do
+		for x=1, #map[y] do
+			if map[y][x] == 1 then
+				love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
+			end
+		end
+	end
+  end
+
+
+
+  local function updateTarget(dt)
+    target.x, target.y = cam1:toWorld(player.grid_x, player.grid_y)
+  end
+
+
+
+  local function updateCameras(dt)
+    cam1:setPosition(player.act_x, player.act_y)
+    cam2:setPosition(player.act_x, player.act_y)
+  
+    local scaleFactor = isDown('[') and -0.8 or (isDown(']') and 0.8 or 0)
+    cam1:setScale(cam1:getScale() + scaleFactor * dt)
+  
+    local angleFactor = isDown(',') and -0.8 or (isDown('.') and 0.8 or 0)
+    cam1:setAngle(cam1:getAngle() + angleFactor * dt)
+  end
+
+
+
+  local function drawCam1ViewPort()
+    love.graphics.setColor(0,0,255,100)
+    love.graphics.rectangle('line', cam1:getVisible())
+  
+    love.graphics.setColor(255,255,255, 100)
+    love.graphics.polygon('line', cam1:getVisibleCorners())
+  end
+
+  local function drawPlayer()
+    love.graphics.setColor(255,255,255,100)
+    love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
+	for y=1, #map do
+		for x=1, #map[y] do
+			if map[y][x] == 1 then
+				love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
+			end
+		end
+    end
+  end
+
+  local function updatePlayer(dt)
+    player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+    player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+  end
 
 function love.load()
-    
-    
-    
-    player = {
+
+    world  = { w = 5000, h = 3000, rows = 10, columns = 20 }
+    --target = { x = 500,  y = 500 }
+
+    player = 
+    {
 		grid_x = 256,
 		grid_y = 256,
 		act_x = 200,
@@ -14,7 +97,8 @@ function love.load()
 		speed = 10
     }
 
-	map = {
+    map = 
+    {
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
@@ -42,57 +126,70 @@ function love.load()
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
-	}
-end
- 
-function love.update(dt)
-	player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
-    player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+    }
     
-end
- 
-function love.draw()
-    
-    --cam:setPosition(player.grid_x, player.grid_y)
-    
-    love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
-	for y=1, #map do
-		for x=1, #map[y] do
-			if map[y][x] == 1 then
-				love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
-			end
-		end
-    end
-    
-    cam:draw(function(l,t,w,h)
-        cam:setPosition(player.act_x, player.grid_y)
-        cam:setScale(2.0)
-      end)
+
+  cam1 = gamera.new(0, 0, world.w, world.h)
+  cam1:setWindow(10,10,520,580)
+
+  cam2 = gamera.new(0,0, world.w, world.h)
+  cam2:setWindow(540,10,250,180)
+  cam2:setScale(0)
 
 end
  
+
+
+function love.update(dt)
+    updatePlayer(dt)
+    updateCameras(dt)
+    --updateTarget(dt)
+end
+ 
+
+
+function love.draw()
+ 
+    cam1:draw(function(l,t,w,h)
+        drawWorld(l,t,w,h)
+        drawPlayer()
+        --drawTarget()
+      end)
+    
+      cam2:draw(function(l,t,w,h)
+        drawWorld(l,t,w,h)
+        drawPlayer()
+        --drawTarget()
+        drawCam1ViewPort()
+      end)
+
+      love.graphics.setColor(255,255,255)
+      love.graphics.rectangle('line', cam1:getWindow())
+      love.graphics.rectangle('line', cam2:getWindow())
+
+end
+ 
+
+
 function love.keypressed(key)
 	if key == "up" then
 		if testMap(0, -1) then
             player.grid_y = player.grid_y - 32
-            --cam:setPosition(player.grid_x, player.grid_y)
 		end
 	elseif key == "down" then
 		if testMap(0, 1) then
             player.grid_y = player.grid_y + 32
-            --cam:setPosition(player.grid_x, player.grid_y)
 		end
 	elseif key == "left" then
 		if testMap(-1, 0) then
             player.grid_x = player.grid_x - 32
-            --cam:setPosition(player.grid_x, player.grid_y)
 		end
 	elseif key == "right" then
 		if testMap(1, 0) then
             player.grid_x = player.grid_x + 32
-            --cam:setPosition(player.grid_x, player.grid_y)
 		end
-	end
+    elseif key == 'escape' then love.event.quit()
+    end
 end
  
 function testMap(x, y)
@@ -101,3 +198,4 @@ function testMap(x, y)
 	end
 	return true
 end
+  
